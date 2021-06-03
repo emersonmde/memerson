49473b1916e9c6c4d4958c79a7290ff0f8c6d286
Matthew Emerson <emersonmde@protonmail.com>
2021-06-02 21:10:11 -0400

    Added DMARC record

--- infrastructure/lib/route53-stack.ts
+++ infrastructure/lib/route53-stack.ts
@@ -13,13 +13,22 @@ export class MemersonRoute53Stack extends cdk.Stack {
         zoneName: 'memerson.net'
     });
 
-    new route53.TxtRecord(this, 'ProtonMailTxtRecord', {
+    new route53.TxtRecord(this, 'MemersonTxtRecord', {
         zone: this.hostedZone,
+        recordName: '',
         values: [
             'protonmail-verification=12f676345e1db386f504a9bbd2b52ce906a3ccdd',
             'v=spf1 include:_spf.protonmail.ch mx ~all',
         ],
     });
+
+    // new route53.TxtRecord(this, 'ProtonMailDmarcRecord', {
+    //     zone: this.hostedZone,
+    //     recordName: '_dmarc',
+    //     values: [
+    //         'v=DMARC1; p=none; rua=mailto:mail@memerson.net',
+    //     ],
+    // });
 
     new route53.MxRecord(this, 'ProtonMailMxRecord', {
         zone: this.hostedZone,
@@ -33,7 +42,25 @@ export class MemersonRoute53Stack extends cdk.Stack {
                 priority: 20
             },
         ]
-        
+    });
+
+    new route53.CnameRecord(this, 'ProtonMailDkimRecord1', {
+        zone: this.hostedZone,
+        recordName: 'protonmail._domainkey',
+        domainName: 'protonmail.domainkey.dbmqw4ugiuaymzfaa65feupnggq2i3kjd36pe4soqavlqlozij64a.domains.proton.ch.'
+    });
+
+
+    new route53.CnameRecord(this, 'ProtonMailDkimRecord2', {
+        zone: this.hostedZone,
+        recordName: 'protonmail2._domainkey',
+        domainName: 'protonmail2.domainkey.dbmqw4ugiuaymzfaa65feupnggq2i3kjd36pe4soqavlqlozij64a.domains.proton.ch.'
+    });
+
+    new route53.CnameRecord(this, 'ProtonMailDkimRecord3', {
+        zone: this.hostedZone,
+        recordName: 'protonmail3._domainkey',
+        domainName: 'protonmail3.domainkey.dbmqw4ugiuaymzfaa65feupnggq2i3kjd36pe4soqavlqlozij64a.domains.proton.ch.'
     });
 
     const nameservers = this.hostedZone.hostedZoneNameServers as string[];
