6334b884c47b5ed45a9aaf50cc5d99694be29c45
Matthew Emerson <emersonmde@protonmail.com>
2021-10-16 20:38:06 -0400

    Add cognito stack

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
-import { AmplifyStack } from '../lib/amplify-stack';
+// import { AmplifyStack } from '../lib/amplify-stack';
 import { Route53Stack } from '../lib/route53-stack';
 import { PipelineStack } from '../lib/pipeline-stack';
 import { CloudFrontStack } from '../lib/cloudfront-stack';
+import { CognitoStack } from '../lib/cognito-stack';
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
@@ -11,6 +12,8 @@ import { CloudFrontStack } from '../lib/cloudfront-stack';
 
 const app = new cdk.App();
 const route53Stack = new Route53Stack(app, 'MemersonRoute53Stack', {});
+
+// TODO: replace amplify with custom infrastructure
 // const amplifyStack = new AmplifyStack(app, 'MemersonAmplifyStack', {
 //   hostedZone: route53Stack.hostedZone
 // });
@@ -26,3 +29,5 @@ new PipelineStack(app, 'MemersonPipelineStack', {
   autoDeployedStacks: autoDeployedStages,
   websiteAssetsBucket: cloudfrontStack.assetsBucket,
 });
+
+new CognitoStack(app, 'MemersonCognitoStack', {});
