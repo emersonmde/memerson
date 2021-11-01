2525ba616d4d5980c0e3478dacbe7f3a5b0baa0e
Matthew Emerson <emersonmde@protonmail.com>
2021-11-01 09:58:36 -0400

    Added css to home page

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,18 +1,22 @@
-import { Typography } from "@material-ui/core";
 import React from "react";
 
 export default class Home extends React.Component<{}, {}> {
+  readonly textStyle: any;
   constructor(props: {}) {
     super(props);
     this.state = {
     }
+
+    this.textStyle = {
+      marginLeft: '10px'
+    };
   }
 
   render() {
     return (
-      <p>
+      <div style={this.textStyle}>
         <pre>Unfortunately, this is probably not what you're looking for.</pre>
-      </p>
+      </div>
     );
   }
 }
