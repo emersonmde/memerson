cdc709c80622cd5f20b4d95598d8d2a43f92f3be
Matthew Emerson <emersonmde@protonmail.com>
2022-07-10 12:42:02 -0400

    Updated server status and control lambdas

--- infrastructure/lambda/server_control.py
+++ infrastructure/lambda/server_control.py
@@ -17,45 +17,52 @@ def server_control_handler(event, context):
             "origin") or "https://memerson.dev, http://localhost:3000",
         "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
     }
+    try:
+        if "queryStringParameters" not in event or event["queryStringParameters"] is None:
+            return {
+                "statusCode": 400,
+                "headers": response_headers,
+                "body": json.dumps({"error": f"Must provide action and instance_id"})
+            }
 
-    if "queryStringParameters" not in event or event["queryStringParameters"] is None:
-        return {
-            "statusCode": 400,
+        action = event["queryStringParameters"].get("action")
+        instance_id = event["queryStringParameters"].get("instance_id")
+
+        if action is None or action == "" or instance_id is None or instance_id == "":
+            return {
+                "statusCode": 400,
+                "headers": response_headers,
+                "body": json.dumps({"error": f"Must provide action and instance_id"})
+            }
+
+        if action == "stop":
+            result = stop_instance(event["queryStringParameters"]["instance_id"])
+        elif action == "start":
+            result = start_instance(event["queryStringParameters"]["instance_id"])
+        else:
+            return {
+                "statusCode": 400,
+                "headers": response_headers,
+                "body": json.dumps({"error": f"Unknown action: {action}"})
+            }
+
+        response = {
+            "statusCode": 200,
             "headers": response_headers,
-            "error": f"Must provide action and instance_id"
+            "body": json.dumps({
+                "servers": result
+            })
         }
-
-    action = event["queryStringParameters"].get("action")
-    instance_id = event["queryStringParameters"].get("instance_id")
-
-    if action is None or action == "" or instance_id is None or instance_id == "":
+        logger.info(f"Response: {response}")
+        return response
+    except Exception as e:
         return {
-            "statusCode": 400,
-            "headers": response_headers,
-            "error": f"Must provide action and instance_id"
+            'statusCode': 500,
+            'headers': response_headers,
+            'body': json.dumps({
+                'error': f'Error: {str(e)}'
+            })
         }
-
-    if action == "stop":
-        result = stop_instance(event["queryStringParameters"]["instance_id"])
-    elif action == "start":
-        result = start_instance(event["queryStringParameters"]["instance_id"])
-    else:
-        return {
-            "statusCode": 400,
-            "headers": response_headers,
-            "error": f"Unknown action: {action}"
-        }
-
-    response = {
-        "statusCode": 200,
-        "headers": response_headers,
-        "body": {
-            "servers": json.dumps(result, default=str)
-        }
-    }
-    logger.info(f"Response: {response}")
-    return response
-
 
 def stop_instance(instancei_id: str):
     response = ec2.stop_instances(
--- infrastructure/lambda/server_status.py
+++ infrastructure/lambda/server_status.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import socket
 from collections import defaultdict
 
 import boto3
@@ -8,30 +9,74 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 ec2 = boto3.client('ec2')
 
+PORT_TEST_TIMEOUT = 2
 
 
 def server_status_handler(event, context):
     logger.info(f'Event: {event}')
-    headers = {
+    response_headers = {
         "Access-Control-Allow-Headers": 'Content-Type',
         "Access-Control-Allow-Origin": event['headers'].get(
             'origin') or 'https://memerson.dev, http://localhost:3000',
         "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
     }
 
-    response = {
-        'statusCode': 200,
-        'headers': headers,
-        'body': {
-            # TODO: "Object of type datetime is not JSON serializable"
-            'servers': json.dumps(server_status(), default=str)
+    try:
+        response = {
+            'statusCode': 200,
+            'headers': response_headers,
+            'body': json.dumps({
+                'servers': get_instances()
+            })
         }
-    }
-    logger.info(f'Response: {response}')
-    return response
+        logger.info(f'Response: {response}')
+        return response
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'headers': response_headers,
+            'body': json.dumps({
+                'error': f'Error: {str(e)}'
+            })
+        }
 
 
-def server_status():
+def get_instances():
     response = ec2.describe_instances()
+    instances = []
+    for reservation in response['Reservations']:
+        if "Instances" not in reservation or not reservation["Instances"]:
+            break
+        for instance in reservation["Instances"]:
+            dns_name = instance.get('PublicDnsName')
+            ip_address = instance.get('PublicIpAddress')
+            instances.append({
+                'instance_id': instance['InstanceId'],
+                'state': instance['State']['Name'],
+                'public_dns_name': dns_name,
+                'public_ip_address': ip_address,
+                'is_minecraft_running': is_minecraft_running(instance['PublicIpAddress']) if ip_address else False,
+                'is_bedrock_running': is_bedrock_running(instance['PublicIpAddress']) if ip_address else False
+            })
     logger.info(f"Instances: {response}")
-    return response
+    return instances
+
+
+def is_minecraft_running(host: str) -> bool:
+    return is_port_open(host, 25565)
+
+
+def is_bedrock_running(host: str) -> bool:
+    return is_port_open(host, 19132, socket.SOCK_DGRAM)
+
+
+def is_port_open(host: str, port: int, socket_type: int = socket.SOCK_STREAM) -> bool:
+    sock = socket.socket(socket.AF_INET, socket_type)
+    sock.settimeout(PORT_TEST_TIMEOUT)
+    connect_result = sock.connect_ex((host, port))
+    if connect_result == 0:
+        # success
+        return True
+    else:
+        # fail
+        return False
