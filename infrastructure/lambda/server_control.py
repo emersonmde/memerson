c96978c6ca694eebda7666978628269f7c5673cd
Matthew Emerson <emersonmde@protonmail.com>
2022-07-10 22:01:32 -0400

    Reformatting files

--- infrastructure/lambda/server_control.py
+++ infrastructure/lambda/server_control.py
@@ -2,58 +2,75 @@ import json
 import logging
 
 import boto3
-from typing import List
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-ec2 = boto3.client("ec2")
+ec2 = boto3.client('ec2')
 
 
 def server_control_handler(event, context):
-    logger.info(f"Event: {event}")
+    """
+    Handles API Gateway event for /server_status
+
+    Example:
+    {
+      "path": "/server_status",
+      "httpMethod": "POST",
+      "isBase64Encoded": true,
+      "queryStringParameters": {
+        "action": "start" | "stop",
+        "instance_id": "<ec2_instance_id>"
+      },
+    }
+    :param event: API Gateway Event, requires queryStringParamters with action ('start' or 'stop')
+        and the EC2 instance_id
+    :param context: Lambda context
+    :return: Response from start_instance or stop_instance
+    """
+    logger.info(f'Event: {event}')
     response_headers = {
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Origin": event["headers"].get(
-            "origin") or "https://memerson.dev, http://localhost:3000",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': event['headers'].get(
+            'origin') or 'https://memerson.dev, http://localhost:3000',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
     }
     try:
-        if "queryStringParameters" not in event or event["queryStringParameters"] is None:
+        if 'queryStringParameters' not in event or event['queryStringParameters'] is None:
             return {
-                "statusCode": 400,
-                "headers": response_headers,
-                "body": json.dumps({"error": f"Must provide action and instance_id"})
+                'statusCode': 400,
+                'headers': response_headers,
+                'body': json.dumps({'error': f'Must provide action and instance_id'})
             }
 
-        action = event["queryStringParameters"].get("action")
-        instance_id = event["queryStringParameters"].get("instance_id")
+        action = event['queryStringParameters'].get('action')
+        instance_id = event['queryStringParameters'].get('instance_id')
 
-        if action is None or action == "" or instance_id is None or instance_id == "":
+        if action is None or action == '' or instance_id is None or instance_id == '':
             return {
-                "statusCode": 400,
-                "headers": response_headers,
-                "body": json.dumps({"error": f"Must provide action and instance_id"})
+                'statusCode': 400,
+                'headers': response_headers,
+                'body': json.dumps({'error': f'Must provide action and instance_id'})
             }
 
-        if action == "stop":
-            result = stop_instance(event["queryStringParameters"]["instance_id"])
-        elif action == "start":
-            result = start_instance(event["queryStringParameters"]["instance_id"])
+        if action == 'stop':
+            result = stop_instance(event['queryStringParameters']['instance_id'])
+        elif action == 'start':
+            result = start_instance(event['queryStringParameters']['instance_id'])
         else:
             return {
-                "statusCode": 400,
-                "headers": response_headers,
-                "body": json.dumps({"error": f"Unknown action: {action}"})
+                'statusCode': 400,
+                'headers': response_headers,
+                'body': json.dumps({'error': f'Unknown action: {action}'})
             }
 
         response = {
-            "statusCode": 200,
-            "headers": response_headers,
-            "body": json.dumps({
-                "servers": result
+            'statusCode': 200,
+            'headers': response_headers,
+            'body': json.dumps({
+                'servers': result
             })
         }
-        logger.info(f"Response: {response}")
+        logger.info(f'Response: {response}')
         return response
     except Exception as e:
         return {
@@ -65,16 +82,28 @@ def server_control_handler(event, context):
         }
 
 def stop_instance(instancei_id: str):
+    """
+    Call stop_instance() for an EC2 instance_id
+
+    :param instancei_id: Instance ID
+    :return: stop_instance() response
+    """
     response = ec2.stop_instances(
         InstanceIds=[instancei_id],
     )
-    logger.info(f"Stop Instances: {response}")
+    logger.info(f'Stop Instances: {response}')
     return response
 
 
 def start_instance(instancei_id: str):
+    """
+    Call stop_instance() for an EC2 instance_id
+
+    :param instancei_id: Instance ID
+    :return: stop_instance() response
+    """
     response = ec2.start_instances(
         InstanceIds=[instancei_id],
     )
-    logger.info(f"Start Instances: {response}")
+    logger.info(f'Start Instances: {response}')
     return response
--- infrastructure/lambda/server_status.py
+++ infrastructure/lambda/server_status.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import socket
-from collections import defaultdict
 
 import boto3
 
@@ -13,12 +12,41 @@ PORT_TEST_TIMEOUT = 2
 
 
 def server_status_handler(event, context):
+    """
+    Handles API Gateway event for /server_status
+
+    Example:
+    {
+      "path": "/server_status",
+      "httpMethod": "POST",
+      "isBase64Encoded": true,
+      "queryStringParameters": {
+        "key": "value",
+      },
+    }
+    :param event: API Gateway Event
+    :param context: Lambda context
+    :return: List of servers (EC2 Instances)
+        {
+            'body': {
+                'servers': [
+                    {
+                        'instance_id': '', # EC2 instance id
+                        'state': '', # EC@ instance state
+                        'public_dns_name': '', # EC2 default public DNS name
+                        'public_ip_address': '', # Public IP address
+                        'is_minecraft_running': '', # True if Minecraft Java port is open
+                        'is_bedrock_running': '', # True if Minecraft Bedrock port is open
+                    }
+                ]
+            }
+    """
     logger.info(f'Event: {event}')
     response_headers = {
-        "Access-Control-Allow-Headers": 'Content-Type',
-        "Access-Control-Allow-Origin": event['headers'].get(
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': event['headers'].get(
             'origin') or 'https://memerson.dev, http://localhost:3000',
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
     }
 
     try:
@@ -32,6 +60,7 @@ def server_status_handler(event, context):
         logger.info(f'Response: {response}')
         return response
     except Exception as e:
+        # Catch all exceptions to include required headers in the response
         return {
             'statusCode': 500,
             'headers': response_headers,
@@ -42,12 +71,17 @@ def server_status_handler(event, context):
 
 
 def get_instances():
+    """
+    Gets all EC2 instances using `describe_instances`
+
+    :return: List of server details
+    """
     response = ec2.describe_instances()
     instances = []
     for reservation in response['Reservations']:
-        if "Instances" not in reservation or not reservation["Instances"]:
+        if 'Instances' not in reservation or not reservation['Instances']:
             break
-        for instance in reservation["Instances"]:
+        for instance in reservation['Instances']:
             dns_name = instance.get('PublicDnsName')
             ip_address = instance.get('PublicIpAddress')
             instances.append({
@@ -58,19 +92,39 @@ def get_instances():
                 'is_minecraft_running': is_minecraft_running(instance['PublicIpAddress']) if ip_address else False,
                 'is_bedrock_running': is_bedrock_running(instance['PublicIpAddress']) if ip_address else False
             })
-    logger.info(f"Instances: {response}")
+    logger.info(f'Instances: {response}')
     return instances
 
 
 def is_minecraft_running(host: str) -> bool:
+    """
+    Tests if the Minecraft default Java server port is open
+
+    :param host: Target minecraft hostname or IP
+    :return: True if the port is open False otherwise
+    """
     return is_port_open(host, 25565)
 
 
 def is_bedrock_running(host: str) -> bool:
+    """
+    Tests if the Minecraft bedrock server default port is open
+
+    :param host: Target minecraft hostname or IP
+    :return: True if the port is open False otherwise
+    """
     return is_port_open(host, 19132, socket.SOCK_DGRAM)
 
 
 def is_port_open(host: str, port: int, socket_type: int = socket.SOCK_STREAM) -> bool:
+    """
+    Tests if a port is open with a short timeout
+
+    :param host: Target hostname or IP
+    :param port: Target port
+    :param socket_type: socket.SOCK_STREAM (default) for TCP or socket.SOCK_DGRAM for UDP
+    :return: True if the port is open False otherwise
+    """
     sock = socket.socket(socket.AF_INET, socket_type)
     sock.settimeout(PORT_TEST_TIMEOUT)
     connect_result = sock.connect_ex((host, port))
